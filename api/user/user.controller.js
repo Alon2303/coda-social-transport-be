f9d8b10be7648068ec52c26ad58af1bb9de6438a
dudu <dudvil1@gmail.com>
2020-11-09 13:56:06 +0200

    logIn commit

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,4 +1,7 @@
 const userService = require("./user.service");
+const mongoose = require('mongoose');
+const User = require('../../models/User');
+const db = require('../../services/dbQuerys/UserQuerys');
 
 
 async function getAllUsersData(req, res) {
@@ -12,6 +15,46 @@ async function getUserByEmail(req, res) {
     console.log("user", user)
     res.send(user);
 }
+
+exports.singUp = async (req, res, next) => {
+  try {
+    const user = await db.findUserByEmail(req.body.email);
+    if (user)
+      return res.status(401).json({
+        message: "user already exist,try again",
+      });
+
+    await db.addUser(req.body);
+    return res.status(201).json({
+      message: "User Created Successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.singIn = async (req, res, next) => {
+  try {
+      const user = await db.findUserByEmail(req.body.email);
+      if (!user) return res.status(400).message({
+          message: "not exist"
+      })
+
+      const password = db.checkPassword(user.password,req.body.password);
+      if(!password) return res.status(400).message({
+        message: "worng password",
+      });
+
+      const token = 'token';
+      return res.status(201).message({
+          message: "ok",
+          token: token
+      })
+      
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
     getAllUsersData,
